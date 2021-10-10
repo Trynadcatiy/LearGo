1377277baa1a43b29142f142e10940fb0e5b8ac8
trynadcatiy <p.voronin@docsinbox.ru>
2021-10-10 22:35:23 +0300

    lesson 3 https://youtu.be/n18K8kCaXyg?list=PL0lO_mIqDDFXXqMzFOIGIb7FOprmUQ_tt

--- learnGo.go
+++ learnGo.go
@@ -13,13 +13,18 @@ type User struct {
 	happiness float64
 }
 
-func (u User) getAllInfo() string {
+func (u *User) getAllInfo() string {
 	return fmt.Sprintf("User name is: %s. "+
 		"He is %d age and he has money equal: %d", u.name, u.age, u.money)
 }
 
+func (u *User) setNewName(newName string) {
+	u.name = newName
+}
+
 func homePage(writer http.ResponseWriter, r *http.Request) {
 	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	bob.setNewName("Alex")
 	fmt.Fprintf(writer, bob.getAllInfo())
 }
 
